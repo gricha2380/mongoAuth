cafec2942374e645f10d18a5ec3c1f75993188f3
Gregor Richardson <gregor.richardson@gmail.com>
2018-03-08 20:02:28 -0800

    starting menu script

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
+app.listen(5000, function () {
+  console.log('Express app listening on port 5000');
 });
